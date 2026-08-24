8ddd2c3b3a1abc643fa377d538875f2dbaf14f5a
안치숙 <songweddin@naver.com>
2026-08-24 23:53:53 +0900

    Chore: ghcr로 오라클에 배포 테스트

--- src/main/java/com/example/data_collector/TestController.java
+++ src/main/java/com/example/data_collector/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String home() {
-        return "Hi";
+        return "Hello";
     }
 }
